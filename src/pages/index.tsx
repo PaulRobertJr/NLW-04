8b1d199c06584b741a7bf3bb15e1543a6bcc6726
Robert <paul_bialli@hotmail.com>
2021-03-01 10:56:42 -0300

    feat: added new component for provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { ChallengeBox } from '../components/ChallengeBox'
 import { Countdown } from '../components/Countdown'
 import { Profile } from '../components/Profile'
+
+import styles from '../styles/pages/Home.module.css'
 import { CountdownProvider } from '../contexts/CountdownContext'
 import { ChallengesProvider } from '../contexts/ChallengesContext'
-import styles from '../styles/pages/Home.module.css'
 
 interface HomeProps {
   level: number;
